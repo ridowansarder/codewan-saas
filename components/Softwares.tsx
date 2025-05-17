b1ec8bcb478c869d28847bbf6d4f20e6aadbb215
ridowansarder <ridwanzuraiz@gmail.com>
2025-05-17 16:55:09 +0600

    Update Softwares.tsx

--- components/Softwares.tsx
+++ components/Softwares.tsx
@@ -14,7 +14,7 @@ const Softwares = () => {
       </div>
       <div className="w-[80%] mx-auto mt-8 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10">
         <div className="flex flex-col items-start space-y-3 my-8 sm:space-y-0 gap-6 bg-emerald-100 dark:bg-gray-800 p-6 rounded-lg">
-          <Goal className="w-12 h-12" />
+          <Goal className="w-12 h-12 dark:bg-amber-400" />
           <div className="flex-1">
             <h1 className="text-xl lg:text-2xl font-bold">Free Intregation</h1>
             <p className="mt-2 w-[70%] text-lg">
@@ -27,7 +27,7 @@ const Softwares = () => {
           </div>
         </div>
         <div className="flex flex-col items-start space-y-3 my-8 sm:space-y-0 gap-6 bg-emerald-100 dark:bg-gray-800 p-6 rounded-lg">
-          <ChartNoAxesColumn className="w-12 h-12" />
+          <ChartNoAxesColumn className="w-12 h-12 dark:bg-amber-400" />
           <div className="flex-1">
             <h1 className="text-xl lg:text-2xl font-bold">
               Product Sales Software
@@ -42,7 +42,7 @@ const Softwares = () => {
           </div>
         </div>
         <div className="flex flex-col items-start space-y-3 my-8 sm:space-y-0 gap-6 bg-emerald-100 dark:bg-gray-800 p-6 rounded-lg">
-          <Cpu className="w-12 h-12" />
+          <Cpu className="w-12 h-12 dark:bg-amber-400" />
           <div className="flex-1">
             <h1 className="text-xl lg:text-2xl font-bold">Best Marketing Software</h1>
             <p className="mt-2 w-[70%] text-lg">
